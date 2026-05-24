24aff6fa111abc1ead5458420010a4be3b2afadd
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2026-05-23 23:00:03 -0500

    feat: innecesary SEO service deleted

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { SeoService } from '@src/app/core/seo/seo.service';
 import { Footer } from '@src/app/shared/footer/footer';
 import { Header } from '@src/app/shared/header/header';
 
@@ -10,10 +9,4 @@ import { Header } from '@src/app/shared/header/header';
     imports: [Header, Footer, RouterOutlet],
     templateUrl: './app.html',
 })
-export class App implements OnInit {
-    private readonly seo = inject(SeoService);
-
-    ngOnInit(): void {
-        this.seo.applyDefaults();
-    }
-}
+export class App {}
