cb278d52564a886e1a3a272f0afd8a489083eb85
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2026-05-23 22:13:43 -0500

    feat: SEO configurations added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SeoService } from '@src/app/core/seo/seo.service';
 import { Footer } from '@src/app/shared/footer/footer';
 import { Header } from '@src/app/shared/header/header';
 
@@ -8,6 +9,11 @@ import { Header } from '@src/app/shared/header/header';
     selector: 'app-root',
     imports: [Header, Footer, RouterOutlet],
     templateUrl: './app.html',
-    styleUrl: './app.scss',
 })
-export class App {}
+export class App implements OnInit {
+    private readonly seo = inject(SeoService);
+
+    ngOnInit(): void {
+        this.seo.applyDefaults();
+    }
+}
